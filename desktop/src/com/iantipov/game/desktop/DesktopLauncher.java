5f4d5bbdf1e69deb9b78a68622cf5c35c1bd928a
PUSHOK\Iprit <vaisdreik@gmail.com>
2018-10-19 13:20:10 +0300

    Create libgdx project and configure android studio

---
+++ desktop/src/com/iantipov/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.iantipov.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.iantipov.game.JavaAndroidGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JavaAndroidGame(), config);
+	}
+}
