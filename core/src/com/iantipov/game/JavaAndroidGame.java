09a449f094e3f9bc3b557b5e57385aea6a5aa2b0
PUSHOK\Iprit <vaisdreik@gmail.com>
2018-10-19 13:23:25 +0300

    add image and set as background

--- core/src/com/iantipov/game/JavaAndroidGame.java
+++ core/src/com/iantipov/game/JavaAndroidGame.java
@@ -13,15 +13,15 @@ public class JavaAndroidGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("space.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0.1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 10, 0);
 		batch.end();
 	}
 	
